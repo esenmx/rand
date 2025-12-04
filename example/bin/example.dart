30312e5b7350712d521a8b9b0fe0eabd55df2c24
Mehmet Esen <43244505+esenmx@users.noreply.github.com>
2025-12-04 08:33:54 +0300

    chore: update dependencies and formatting

--- example/bin/example.dart
+++ example/bin/example.dart
@@ -1,23 +1,46 @@
+// ignore_for_file: avoid_print
+
 import 'package:rand/rand.dart';
 
 void main() {
-  /// Random [DateTime] based on unix epoch
-  print(Rand.dateTime());
+  final divider = ''.padLeft(45, '=');
 
-  /// a random moment in my life
-  print(Rand.dateTime(DateTime(1990, 6, 26, 8, 30), DateTime.now()));
+  print('\x1B[34m$divider\nRand Demo Output\n$divider\x1B[0m\n');
 
-  print(Rand.weightedRandomizedArray(
-    weights: [80, 10, 10],
-    pool: [60, 90, 30],
-    size: 1,
-  ).first);
+  {
+    final dateTime = Rand.dateTime();
+    print('\x1B[32m[Random DateTime]\x1B[0m');
+    print('  $dateTime\n');
+  }
 
-  print(Rand.weightedRandomizedArray(
-    weights: [10, 40, 50, 10],
-    pool: _Position.values,
-    size: 11,
-  ));
+  {
+    final myLifeMoment =
+        Rand.dateTime(DateTime(1990, 6, 26, 8, 30), DateTime.now());
+    print('\x1B[32m[Random moment in my life]\x1B[0m');
+    print('  $myLifeMoment\n');
+  }
+
+  {
+    final weightedNumber = Rand.weightedRandomizedArray(
+      weights: [80, 10, 10],
+      pool: [60, 90, 30],
+      size: 1,
+    ).first;
+    print('\x1B[32m[Weighted Random Number]\x1B[0m');
+    print('  $weightedNumber\n');
+  }
+
+  {
+    final weightedPositions = Rand.weightedRandomizedArray(
+      weights: [10, 40, 50, 10],
+      pool: _Position.values,
+      size: 11,
+    );
+    print('\x1B[32m[Weighted Randomized Football Positions]\x1B[0m');
+    print('  ${weightedPositions.map((e) => e.name).toList()}\n');
+  }
+
+  print('\x1B[34m$divider\x1B[0m');
 }
 
 enum _Position { gk, def, mid, fwd }
