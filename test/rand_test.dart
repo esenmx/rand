c0ca7e6944449bdc71dc4cff57fcabf3b7b4a986
Mehmet Esen <mehmetesen@proton.me>
2026-06-27 15:06:20 +0300

    style: format test/rand_test.dart

--- test/rand_test.dart
+++ test/rand_test.dart
@@ -271,8 +271,7 @@ void main() {
       check(() => Rand.password(length: 3)).throws<ArgumentError>();
     });
 
-    test(
-        'password guarantees representation of all enabled character sets',
+    test('password guarantees representation of all enabled character sets',
         () {
       const lowercaseSet = 'abcdefghijklmnopqrstuvwxyz';
       const uppercaseSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
